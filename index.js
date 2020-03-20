0d20eb9885c644da6a65564498d08460cb13030a
SebastianRosenquist <seb_rosenquist@hotmail.com>
2020-03-20 17:23:53 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+
+const app = new express();
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const BlogPost = require('./models/BlogPost.js');
+const fileUpload = require('express-fileupload');
+
+app.use(fileUpload());
+
+mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
+
+app.use(express.static('public'));
+
+app.listen(4000, ()=>{
+    console.log('App listening on port 4000 ...')    
+});
+
+const validateMiddleWare = require("./middleware/validationMiddleware");
+
+app.use('/posts/store',validateMiddleWare);
+
+/* app.get('/about',(req,res)=>{
+    res.render('about');
+})
+
+app.get('/contact',(req,res)=>{        
+    res.render('contact');
+})
+
+app.get('/post',(req,res)=>{    
+    res.render('post')
+}) */
+
+const newPostController = require('./controllers/newPost');
+const homeController = require('./controllers/home.');
+const storePostController = require('./controllers/storePost');
+const getPostController = require('./controllers/getPost');
+
+app.get('/', homeController);
+
+app.get('/post/:id', getPostController);
+
+app.post('/posts/store', storePostController);
+
+app.get('/posts/new',newPostController);
